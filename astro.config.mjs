d68a8023c338d85a07cbe7a384305382dd4c28ff
Chen Hui Jing <1461498+huijing@users.noreply.github.com>
2024-10-09 05:01:58 -0400

    feat: add open payments 101 content and multi-sidebar (#17)
    * feat: add open payments 101 content and multi-sidebar
    
    * Update peer dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
+import starlightUtils from "@lorenzo_lewis/starlight-utils";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,13 @@ export default defineConfig({
     starlight({
       title: "Interledger Learning Hub",
       description: "Enable seamless exchange of value across payment networks.",
+      plugins: [
+        starlightUtils({
+          multiSidebar: {
+            switcherStyle: "dropdown",
+          },
+        }),
+      ],
       logo: {
         dark: "./src/assets/logo_color-white_foundation.svg",
         light: "./src/assets/logo_color-black_foundation.svg",
@@ -25,32 +33,69 @@ export default defineConfig({
         instagram: "https://www.instagram.com/interledgerfoundation/",
         linkedin: "https://www.linkedin.com/company/interledger-foundation/",
         mastodon: "https://interledger.social/about",
-        slack:
-          "https://communityinviter.com/apps/interledger/interledger-working-groups-slack",
+        slack: "https://communityinviter.com/apps/interledger/interledger-working-groups-slack",
         "x.com": "https://twitter.com/interledger",
         youtube: "https://www.youtube.com/@InterledgerFoundation",
       },
       sidebar: [
-        { label: "Get started", link: "/" },
         {
-          label: "Introduction Unit",
-          collapsed: true,
-          autogenerate: { directory: "introduction" },
+          label: "Interledger 101",
+          items: [
+            { label: "Getting started", link: "interledger-101" },
+            {
+              label: "Introduction Unit",
+              collapsed: true,
+              autogenerate: { directory: "interledger-101/introduction" },
+            },
+            {
+              label: "Digital Financial Inclusion Unit",
+              collapsed: true,
+              autogenerate: { directory: "interledger-101/digital-financial-inclusion" },
+            },
+            {
+              label: "Building With Interledger Unit",
+              collapsed: true,
+              autogenerate: { directory: "interledger-101/building-with-interledger" },
+            },
+            {
+              label: "Interledger Technology Unit",
+              collapsed: true,
+              autogenerate: { directory: "interledger-101/interledger-technology" },
+            },
+          ],
         },
         {
-          label: "Digital Financial Inclusion Unit",
-          collapsed: true,
-          autogenerate: { directory: "digital-financial-inclusion" },
-        },
-        {
-          label: "Building With Interledger Unit",
-          collapsed: true,
-          autogenerate: { directory: "building-with-interledger" },
-        },
-        {
-          label: "Interledger Technology Unit",
-          collapsed: true,
-          autogenerate: { directory: "interledger-technology" },
+          label: "Open Payments 101",
+          items: [
+            { label: "Open Payments 101", link: "open-payments-101" },
+            {
+              label: "A Simple Guide to the Open Payments Standard",
+              link: "https://interledger.org/developers/blog/simple-open-payments-guide/",
+              attrs: {
+                target: "_blank",
+                rel: "noopener noreferrer",
+                "data-icon": "external",
+              },
+            },
+            {
+              label: "Introduction to Open Payments video series",
+              link: "https://www.youtube.com/watch?v=zxDvtYVhiBE&list=PLDHju0onYcAJakrsF-I7LK_0phEqurn46 ",
+              attrs: {
+                target: "_blank",
+                rel: "noopener noreferrer",
+                "data-icon": "external",
+              },
+            },
+            {
+              label: "Open Payments Docs",
+              link: "https://openpayments.dev/",
+              attrs: {
+                target: "_blank",
+                rel: "noopener noreferrer",
+                "data-icon": "external",
+              },
+            },
+          ],
         },
       ],
     }),
